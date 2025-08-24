0ae8c20d2788045e02d187a1265d84972ad41e48
igorb <igor.barzasekov@gmail.com>
2025-08-24 13:30:20 +0200

    Added new test for order-page.ts

--- tests/pages/login-page.ts
+++ tests/pages/login-page.ts
@@ -9,14 +9,14 @@ export class LoginPage {
   readonly signInButton: Locator
   readonly usernameField: Locator
   readonly passwordField: Locator
-  // add more locators here
+  readonly errorMessage: Locator
 
   constructor(page: Page) {
     this.page = page
     this.signInButton = page.getByTestId('signIn-button')
     this.usernameField = page.getByTestId('username-input')
     this.passwordField = page.getByTestId('password-input')
-    // continue with the rest of the implementation below
+    this.errorMessage = page.getByTestId('authorizationError-popup')
   }
 
   async open() {
@@ -29,6 +29,4 @@ export class LoginPage {
     await this.signInButton.click()
     return new OrderPage(this.page)
   }
-
-  // continue with the rest of the implementation below
 }
