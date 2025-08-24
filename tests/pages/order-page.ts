65d9b703fefa0703a3193b32905f11a820115a7b
igorb <igor.barzasekov@gmail.com>
2025-08-24 14:16:22 +0200

    Added new pages, modified tests, finished lesson 18 adjusted import type

--- tests/pages/order-page.ts
+++ tests/pages/order-page.ts
@@ -1,4 +1,4 @@
-import { Locator, Page } from '@playwright/test'
+import type { Locator, Page } from '@playwright/test'
 import { AuthorizedPage } from './authorized-page'
 
 export class OrderPage extends AuthorizedPage {
