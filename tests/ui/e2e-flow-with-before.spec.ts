75953f78d40bb06f527f17758985ba6df1f2c7ad
igorb <igor.barzasekov@gmail.com>
2025-08-24 14:12:50 +0200

    Added new pages, modified tests, finished lesson 18

--- tests/ui/e2e-flow-with-before.spec.ts
+++ tests/ui/e2e-flow-with-before.spec.ts
@@ -2,6 +2,8 @@ import { test, expect } from '@playwright/test'
 import { LoginPage } from '../pages/login-page'
 import { faker } from '@faker-js/faker/locale/ar'
 import { PASSWORD, USERNAME } from '../../config/env-data'
+import { OrderNotFound } from '../pages/order-not-found'
+import { OrderFound } from '../pages/order-found'
 
 let authPage: LoginPage
 
@@ -9,13 +11,11 @@ test.beforeEach(async ({ page }) => {
   authPage = new LoginPage(page)
   await authPage.open()
 })
-
 test('signIn button disabled when incorrect data inserted', async ({}) => {
   await authPage.usernameField.fill(faker.lorem.word(2))
   await authPage.passwordField.fill(faker.lorem.word(7))
   await expect(authPage.signInButton).toBeDisabled()
 })
-
 test('error message incorrect credentials', async ({}) => {
   await authPage.usernameField.fill(faker.lorem.word(2))
   await authPage.passwordField.fill('testtest')
@@ -25,12 +25,12 @@ test('error message incorrect credentials', async ({}) => {
 
 test('login with correct credentials and verify order creation page', async ({}) => {
   const orderCreationPage = await authPage.signIn(USERNAME, PASSWORD)
-  await expect.soft(orderCreationPage.statusButton).toBeVisible()
+  await expect.soft(orderCreationPage.buttonStatus).toBeVisible()
   await expect.soft(orderCreationPage.orderButton).toBeVisible()
   await expect.soft(orderCreationPage.name).toBeVisible()
   await expect.soft(orderCreationPage.phone).toBeVisible()
   await expect.soft(orderCreationPage.comment).toBeVisible()
-  await expect.soft(orderCreationPage.logoutButton).toBeVisible()
+  await expect.soft(orderCreationPage.buttonLogOut).toBeVisible()
 })
 
 test('login and create order', async ({}) => {
@@ -47,6 +47,33 @@ test('login and create order', async ({}) => {
 
 test('login and logout', async ({}) => {
   const orderCreationPage = await authPage.signIn(USERNAME, PASSWORD)
-  await orderCreationPage.logoutButton.click()
+  await orderCreationPage.buttonLogOut.click()
   await expect.soft(authPage.signInButton).toBeVisible()
+})
+
+test('Order not found page elements are visible', async ({ page }) => {
+  const orderCreationPage = await authPage.signIn(USERNAME, PASSWORD)
+  await orderCreationPage.buttonStatus.click()
+  await orderCreationPage.inputOrderNumber.fill('0000')
+  await orderCreationPage.buttonTrakingOrder.click()
+  const orderNotFoundPage = new OrderNotFound(page)
+  await expect.soft(orderNotFoundPage.orderNotFoundTitle).toBeVisible()
+  await expect.soft(orderNotFoundPage.orderNotFoundMessage).toBeVisible()
+  await expect.soft(orderNotFoundPage.orderNotFoundImage).toBeVisible()
+})
+
+test('Order found page elements are visible', async ({ page }) => {
+  const orderCreationPage = await authPage.signIn(USERNAME, PASSWORD)
+  await orderCreationPage.buttonStatus.click()
+  await orderCreationPage.inputOrderNumber.fill('10430')
+  await orderCreationPage.buttonTrakingOrder.click()
+  const orderFoundPage = new OrderFound(page)
+  await expect.soft(orderFoundPage.input).toBeVisible()
+  await expect.soft(orderFoundPage.orderInformationName).toBeVisible()
+  await expect.soft(orderFoundPage.orderInformationPhone).toBeVisible()
+  await expect.soft(orderFoundPage.orderInformationComment).toBeVisible()
+  await expect.soft(orderFoundPage.orderStatusOpen).toBeVisible()
+  await expect.soft(orderFoundPage.orderStatusAccepted).toBeVisible()
+  await expect.soft(orderFoundPage.orderStatusInProgress).toBeVisible()
+  await expect.soft(orderFoundPage.orderStatusDelivered).toBeVisible()
 })
